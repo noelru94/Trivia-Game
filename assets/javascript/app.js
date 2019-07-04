0c99606c61f1e5e60c6ad211b352617fc42468e6
noelru94 <noelru94@gmail.com>
2019-07-04 14:32:54 -0400

    questions array of objects with values of: question, choiceA, choiceB, choiceC and correct

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+//
+
+var questions = [
+    {
+        question: 'How many continents are there in the world?' ,
+        choiceA: 4,
+        choiceB: 6,
+        choiceC: 8,
+        correct: 7
+    },
+    {
+        question:'How many wholes are there in a full round of golf?' ,
+        choiceA: 12,
+        choiceB: 16,
+        choiceC: 9,
+        correct: 18
+    },
+    {
+        question: 'Which state is called the Golden State?',
+        choiceA: 'New Jersey',
+        choiceB: 'Colorado',
+        choiceC: 'Florida',
+        correct: 'California'
+    }
+];
+
+
+
+
+
+
+$("#start").on('click',function(){
+    $('.jumbotron').css('display','none');
+    
+
+})
+
+
+
+    
+
+
+
+
