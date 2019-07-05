607a189d0121ce783ed5a5c62842e843c8c3c303
noelru94 <noelru94@gmail.com>
2019-07-04 21:26:45 -0400

    renderCounter function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,69 +4,61 @@ var questions = [
     {
         question: 'How many continents are there in the world?' ,
         choiceA: 4,
-        choiceB: 6,
+        choiceB: 7,
         choiceC: 8,
-        correct: 7
+        correct: 'B'
     },
     {
         question:'How many wholes are there in a full round of golf?' ,
         choiceA: 12,
-        choiceB: 16,
+        choiceB: 18,
         choiceC: 9,
-        correct: 18
+        correct: 'B'
     },
     {
         question: 'Which state is called the Golden State?',
-        choiceA: 'New Jersey',
+        choiceA: 'California',
         choiceB: 'Colorado',
         choiceC: 'Florida',
-        correct: 'California'
+        correct: 'A'
     }
 ];
-var lastQuestionIndex = questions.length - 1;
-var runningQuestionIndex = 0;
+var lastQuestion = questions.length - 1;
+var runningQuestion = 0;
 var count = 15;
+var score = 0;
+var TIMER ;
+var correctScore = 0;
+var wrongScore = 0;
+
+$("#start").on('click',function(){
+    $('.start').css('display','none');
+   
+    $('#trivia').css('display','block');
+    TIMER = setInterval(renderCounter,1000);
+    renderQuestion();
+      
+})
 
 
-function questionRender(){
-    var q = questions[runningQuestionIndex];
-    $('#question').html('<p>'+q.question+'</p>');
+function renderQuestion(){
+    var q = questions[runningQuestion];
+    $('#question').html(`<p>${q.question}</p>`);
     $('#A').html(q.choiceA);
     $('#B').html(q.choiceB);
     $('#C').html(q.choiceC);
 }
 
-function counterRender(){
-    if( count != 0 ){
-        $('#question-time').html("Time remaining: " + count);
+
+
+
+function renderCounter(){
+    if( count >= 0 ){
+        $('#count').html(count);
         count--;
     }else{
-        count = 15;
-        wrongAswer();
-        if( runningQuestionIndex < lastQuestionIndex ){
-            runnningQuestionIndex++;
-            questionRender();
-        }
+        count = 15;  
+        nextQuestion();
     }
 }
-
-
-
-
-
-$("#start").on('click',function(){
-    $('.jumbotron').css('display','none');
-    
-    $('#trivia').css('display','block');
-    questionRender();
-    setInterval(counterRender,1000);
-    
-})
-
-
-
-    
-
-
-
 
